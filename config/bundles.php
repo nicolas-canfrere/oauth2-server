1044979a8b42a0f686aebe572e70e65c74b778c4
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2025-09-30 22:42:18 +0200

    feat: start project

--- config/bundles.php
+++ config/bundles.php
@@ -1,5 +1,12 @@
 <?php
 
+declare(strict_types=1);
+
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Jose\Bundle\JoseFramework\JoseFrameworkBundle::class => ['all' => true],
 ];
---
+++ tests/ItWorksTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Tests;
+
+use PHPUnit\Framework\Attributes\Test;
+use PHPUnit\Framework\TestCase;
+
+final class ItWorksTest extends TestCase
+{
+    #[Test]
+    public function cantest(): void
+    {
+        // given
+
+        // when
+
+        // then
+        $this->assertTrue(true);
+    }
+}
