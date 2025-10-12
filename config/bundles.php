9c689363c743f61e78947753c73ca75e8efc04fc
Nicolas Canfrere <nicolas.canfrere@gmail.com>
2025-10-12 07:40:54 +0200

    Revert "tmp: theme with tailwindcss and daisyui"
    This reverts commit 0568bdf580936c7cc49675b59f8ae4554c47abf6.

--- config/bundles.php
+++ config/bundles.php
@@ -13,5 +13,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfonycasts\TailwindBundle\SymfonycastsTailwindBundle::class => ['all' => true],
 ];
